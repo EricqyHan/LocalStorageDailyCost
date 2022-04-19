a60721b549302467ddbb4fe8bb44de102b634bd8
EricQyhan <Ericqyhan@gmail.com>
2022-04-18 21:22:10 -0400

    Added base layer of HTML. Will need more time to research HTML

---
+++ JS/app.js
@@ -0,0 +1,6 @@
+document.querySelector(".submit-daily").addEventListener("click", addItem);
+
+function addItem() {}
+
+// $("#employee-table > tbody").append("<tr><td>" + empName + "</td><td>" + empRole + "</td><td>" +
+// empStartPretty + "</td><td>" + empMonths + "</td><td>" + empRate + "</td><td>" + empBilled + "</td></tr>");
